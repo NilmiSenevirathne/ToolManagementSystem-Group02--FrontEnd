68eec203bed23292830d3b9df988ba324e0738a6
NilmiSenevirathne <126900940+NilmiSenevirathne@users.noreply.github.com>
2024-04-05 22:46:03 +0530

    changes in sidebar.jsx and create new pages

---
+++ toolmanagementsystem/toolsmanagementsystem/src/Position/StockSupervisor/ManageStock/ManageStock.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Sidebar from '../../../Components/Sidebar/Sidebar';
+
+const ManageStock = () => {
+  return (
+    <div>
+        <Sidebar>
+             <div className='stock-content'>
+                 <h1>ManageStock</h1>
+             </div>
+        </Sidebar>
+
+    </div>
+  )
+}
+
+export default ManageStock
---
+++ toolmanagementsystem/toolsmanagementsystem/src/Position/StockSupervisor/Reports/Reports.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Sidebar from '../../../Components/Sidebar/Sidebar';
+
+const Reports = () => {
+  return (
+    <div>
+      
+        <Sidebar>
+             <div className='stock-content'>
+                 <h1>Reports</h1>
+             </div>
+        </Sidebar>
+
+    </div>
+  )
+}
+
+export default Reports
