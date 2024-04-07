adedfeba698fc0e8de5cb79aba2d186dfd5d0b06
anusari <116504727+anusari@users.noreply.github.com>
2024-04-07 21:56:58 +0530

    Update ViewInventory.jsx

---
+++ toolmanagementsystem/toolsmanagementsystem/src/Position/Manager/ViewInventory/ViewInventory.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CreateToolbox = () =>{
+
+    return(
+      
+         <div>
+              <h1> Create Toolbox </h1>
+         </div>
+     
+    );
+};
+export default CreateToolbox;
