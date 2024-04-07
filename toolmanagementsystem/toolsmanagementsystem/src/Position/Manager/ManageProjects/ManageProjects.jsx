300be8dcefdb80ba6da75b2b246a1dbd4ca215cf
anusari <116504727+anusari@users.noreply.github.com>
2024-04-07 21:55:40 +0530

    Update ManageProjects.jsx

---
+++ toolmanagementsystem/toolsmanagementsystem/src/Position/Manager/ManageProjects/ManageProjects.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ManageProjects = () =>{
+    const cat=[
+
+    ]
+
+
+    return(
+      
+         <div>
+              <h1> Manage Projects </h1>
+         </div>
+     
+    );
+};
+export default ManageProjects;
