70e4ee35bd75cf5dc59f02d1b27aefbcb90255a9
anusari <116504727+anusari@users.noreply.github.com>
2024-04-27 18:05:17 +0530

    Update Home.jsx

--- toolmanagementsystem/toolsmanagementsystem/src/Position/Manager/Pages/ManageProjects/Home.jsx
+++ toolmanagementsystem/toolsmanagementsystem/src/Position/Manager/Pages/ManageProjects/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link, useParams } from 'react-router-dom';
-
+import Sidebar from '../../../../Components/ManagerSidebar.jsx';
 
 export default function Home() {
   const [projects, setProjects] = useState([]);
@@ -26,41 +26,46 @@ export default function Home() {
   };
 
   return (
-    
-    <div className='container'>
-     
-      <div className='py-4'>
-        <table className="table border shadow ">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Project id</th>
-              <th scope="col">Description</th>
-              <th scope="col">projectName</th>
-              <th scope="col">supervisor</th>
-              <th scope="col">SiteSupervisor</th>
-              <th scope="col">locationId</th>
-              <th scope="col">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {projects.map((project, index) => (
-              <tr key={index}>
-                <th scope="row">{index + 1}</th>
-                <td>{project.projectId}</td>
-                <td>{project.description}</td>
-                <td>{project.projectName}</td>
-                <td>{project.siteSupervisorID}</td>
-                <td>{project.siteSupervisorName}</td>
-                <td>{project.locationId}</td>
-                <td>
-                  <Link className='btn btn-outline-primary mx-2' to={`/UpdateProjects/${project.projectId}`}>Edit</Link>
-                  <button className='btn btn-danger mx-2' onClick={() => deleteProject(project.projectId)}>Delete</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-lg-3">
+          <Sidebar />
+        </div>
+        <div className="col-lg-9">
+          <div className="py-4">
+            <table className="table border shadow">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Project id</th>
+                  <th scope="col">Description</th>
+                  <th scope="col">projectName</th>
+                  <th scope="col">supervisor</th>
+                  <th scope="col">SiteSupervisor</th>
+                  <th scope="col">locationId</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {projects.map((project, index) => (
+                  <tr key={index}>
+                    <th scope="row">{index + 1}</th>
+                    <td>{project.projectId}</td>
+                    <td>{project.description}</td>
+                    <td>{project.projectName}</td>
+                    <td>{project.siteSupervisorID}</td>
+                    <td>{project.siteSupervisorName}</td>
+                    <td>{project.locationId}</td>
+                    <td>
+                      <Link className='btn btn-outline-primary mx-2' to={`/UpdateProjects/${project.projectId}`}>Edit</Link>
+                      <button className='btn btn-danger mx-2' onClick={() => deleteProject(project.projectId)}>Delete</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   );
