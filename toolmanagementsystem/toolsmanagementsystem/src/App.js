cb0e73534b92ad60a7353156452e3e5a6cad33e0
NilmiSenevirathne <126900940+NilmiSenevirathne@users.noreply.github.com>
2024-04-27 11:52:47 +0530

    fix errors in loginpage and another pages

--- toolmanagementsystem/toolsmanagementsystem/src/App.js
+++ toolmanagementsystem/toolsmanagementsystem/src/App.js
@@ -1,9 +1,8 @@
-
 import React from 'react';
-import { Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LoginForm from '../src/LoginPage/LoginForm.jsx';
-import ManageStock  from './Position/StockSupervisor/ManageStock/ManageStock.jsx';
+import ManageStock from './Position/StockSupervisor/ManageStock/ManageStock.jsx';
 import CreateToolbox from './Position/StockSupervisor/CreateToolBox/CreateToolbox.jsx';
 import TrackToolbox from './Position/StockSupervisor/TrackToolbox/TrackToolbox.jsx';
 import Reports from './Position/StockSupervisor/Reports/Reports.jsx';
@@ -21,56 +20,39 @@ import AddLocation from './Position/Manager/Pages/ManageProjects/Projects/Locati
 import Home from './Position/Manager/Pages/ManageProjects/Home.jsx'
 import Dboard from './Position/Sitesupervisor/dashboard/Dboard.jsx';
 
-
-
 function App() {
- return (
-
-      <>
-    
-            <Routes>
-                    <Route path='/' element={<LoginForm/>}> </Route>
-
-                    {/* Manager pages  */}
-                    <Route path="/managerdashboard" element={<ManagerDashboard/>} />
-                    <Route path="/manageprojects" element={<ManageProjects />} />
-                    <Route path="/ViewInventory" element={<ViewInventory />} />
-                    <Route path="/tracktoolbox" element={<MTrackToolbox />} />
-                    <Route path="/addprojects" element={<AddProjects />} />
-                    <Route path="/UpdateProjects/:project_id" element={<UpdateProjects />} />
-                    <Route path="/AddLocation" element={<AddLocation />} />
-                    <Route path="/locationHome" element={<LocationHome />} />
-                    <Route path="/home" element={<Home/>}/>
-
-
-                     {/* StockSupervisor pages  */}
-                    <Route path='/stocksupervisordashboard' element={<StockSupervisorDashboard/>} />
-                    <Route  path = "/managestock"  element={<ManageStock/>}/>
-                    <Route  path = "/addTool"  element={<AddTool/>}/>
-                    <Route path='/editTool/:toolId' element={<EditTool/>}/>
-                    <Route  path = "/createtoolbox"  element={<CreateToolbox/>}/>
-                    <Route  path = "/tracktoolbox"  element={<TrackToolbox/>}/>
-                    <Route  path = "/reports"  element={<Reports/>}/>
-                      
-                     {/* SiteSupervisor pages */}
-                   <Route path='/supervisordashboard' element={<Dboard/>}> </Route>
-                    
-               </Routes>
-                    
-
-          
-        
+  return (
   
-       </>
-     );
+        <Routes>
+        <Route path='/' element={<LoginForm/>} />
 
+        {/* Manager pages */}
+        <Route path="/managerdashboard" element={<ManagerDashboard/>} />
+        <Route path="/manageprojects" element={<ManageProjects />} />
+        <Route path="/ViewInventory" element={<ViewInventory />} />
+        <Route path="/tracktoolbox" element={<MTrackToolbox />} />
+        <Route path="/addprojects" element={<AddProjects />} />
+        <Route path="/UpdateProjects/:project_id" element={<UpdateProjects />} />
+        <Route path="/AddLocation" element={<AddLocation />} />
+        <Route path="/locationHome" element={<LocationHome />} />
+        <Route path="/home" element={<Home/>}/>
 
+        {/* StockSupervisor pages */}
+        <Route path='/stocksupervisordashboard' element={<StockSupervisorDashboard/>} />
+        <Route path="/managestock" element={<ManageStock/>} />
+        <Route path="/addTool" element={<AddTool/>} />
+        <Route path='/editTool/:toolId' element={<EditTool/>} />
+        <Route path="/createtoolbox" element={<CreateToolbox/>} />
+        <Route path="/tracktoolbox" element={<TrackToolbox/>} />
+        <Route path="/reports" element={<Reports/>} />
+
+        {/* SiteSupervisor pages */}
+        <Route path='/supervisordashboard' element={<Dboard/>} />
+
+        </Routes> 
+      
+  
+  );
 }
 
-
 export default App;
-
-
-
-
-
--- toolmanagementsystem/toolsmanagementsystem/src/LoginPage/LoginForm.jsx
+++ toolmanagementsystem/toolsmanagementsystem/src/LoginPage/LoginForm.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react';
+// LoginForm.js
+import React, { useState } from 'react';
 import './LoginForm.css';
 import Login from '../../src/images/user1.jpg';
 import Validation from '../../src/LoginPage/Validation.js';
-import { FaUser } from "react-icons/fa";
-import { FaLock } from "react-icons/fa";
+import { FaUser, FaLock } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
 
 function LoginForm() {
-    const navigate = useNavigate(); // Using useNavigate hook instead of Navigate component
+    const navigate = useNavigate();
 
     const [values, setValues] = useState({
         username: '',
@@ -15,98 +15,77 @@ function LoginForm() {
     });
 
     const [errors, setErrors] = useState({});
-    
+
     function handleChange(e) {
         setValues({ ...values, [e.target.name]: e.target.value });
     }
 
     function handleSubmit(e) {
-      e.preventDefault();
+        e.preventDefault();
 
-      const validationErrors = Validation(values);
-      setErrors(validationErrors);
+        const validationErrors = Validation(values);
+        setErrors(validationErrors);
 
-      if (Object.keys(validationErrors).length === 0) {
-          fetch('http://localhost:8080/authentication/login', {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(values),
-          })
-              .then(response => {
-                  if (response.ok) {
-                      console.log("Login Success!!");
-                     if((values.username === 'isuru@gmail.com') && (values.password === 'isuru@123') )
-                     {
+        if (Object.keys(validationErrors).length === 0) {
+            fetch('http://localhost:8080/authentication/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(values),
+            })
+            .then(response => {
+                if (response.ok) {
+                    console.log("Login Success!!");
+                    if (values.username === 'isuru@gmail.com' && values.password === 'isuru@123') {
                         navigate("Admin");
-                     }
-                     else if((values.username === 'gagana@gmail.com') && (values.password === 'Gagana@623'))
-                     {
+                    } else if (values.username === 'gagana@gmail.com' && values.password === 'Gagana@623') {
                         navigate("Manager");
-                     }
-                     else if((values.username === 'nimantha@gmail.com') && (values.password === 'Nima#456'))
-                     {
+                    } else if (values.username === 'nimantha@gmail.com' && values.password === 'Nima#456') {
                         navigate("/stocksupervisordashboard");
-                     }
-                     else if((values.username === 'kusal@gmail.com') && (values.password === 'kusal#@8'))
-                     {
+                    } else if (values.username === 'kusal@gmail.com' && values.password === 'kusal#@8') {
                         navigate("Site Supervisor");
-                     }
-                       
-                  } else {
-                      throw new Error('Login failed'); // Throw error for unsuccessful response
-                  }
-              })
-              .then(data => {
-                  // Handle successful login response
-                  console.log(data); // This will be the data returned from backend
-                  // Redirect to dashboard or do something else
-              })
-              .catch(error => {
-                  console.error('Error during login:', error);
-                  // Handle login error, maybe show a message to the user
-              });
-      } else {
-          console.error('Form validation errors:', validationErrors);
-          // Handle form validation errors, maybe display them to the user
-      }
-  }
-     
-     
-  return (
-    <div >
-        <div className='wrapper'>
+                    }
+                } else {
+                    throw new Error('Login failed');
+                }
+            })
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.error('Error during login:', error);
+            });
+        }
+    }
 
-           
-           <form onSubmit={handleSubmit}>
-             
-                <h1 className='name'> Dilum BMK Engineers (Pvt)Ltd. </h1>
-                <img className="englogo" src={Login} alt=''/> 
-                <h1>Login</h1>
+    return (
+        <div>
+            <div className='wrapper'>
+                <form onSubmit={handleSubmit}>
+                    <h1 className='name'> Dilum BMK Engineers (Pvt)Ltd. </h1>
+                    <img className="englogo" src={Login} alt=''/>
+                    <h1>Login</h1>
 
-                <div className='input-box'> 
-                   <label htmlFor='email'>Username</label>
-                   <input type='email' placeholder='Email' value={values.name} name='name' onChange={handleChange} />
-                  {errors.name && <p style= {{color: "red" , fontSize: "13px"}}> {errors.name}</p>}
-                   <FaUser className='icon' />
-                </div>
+                    <div className='input-box'>
+                        <label htmlFor='username'>Username</label>
+                        <input type='email' placeholder='Email' value={values.username} name='username' onChange={handleChange} />
+                        {errors.username && <p style={{ color: "red", fontSize: "13px" }}>{errors.username}</p>}
+                        <FaUser className='icon' />
+                    </div>
 
-                <div className='input-box'> 
-                   <label htmlFor='password'>Password</label>
-                   <input type='password' placeholder='password' value= {values.password} name='password' onChange={handleChange}/>
-                   {errors.password && <p style= {{color: "red" , fontSize: "13px"}}> {errors.password}</p>}
-                   <FaLock className='icon'/>
-                </div>
-                
-                    <button className='submit'>Login</button>
-                
-                
-            
-           </form>
+                    <div className='input-box'>
+                        <label htmlFor='password'>Password</label>
+                        <input type='password' placeholder='password' value={values.password} name='password' onChange={handleChange} />
+                        {errors.password && <p style={{ color: "red", fontSize: "13px" }}>{errors.password}</p>}
+                        <FaLock className='icon' />
+                    </div>
+
+                    <button className='submit' type="submit">Login</button>
+                </form>
+            </div>
         </div>
-    </div>
-  )
+    );
+}
 
-  }
-export default LoginForm
+export default LoginForm;
