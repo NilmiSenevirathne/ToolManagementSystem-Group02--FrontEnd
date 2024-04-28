24bb4647279d0360dd337526968952694a8f6a69
anusari <116504727+anusari@users.noreply.github.com>
2024-04-29 01:08:58 +0530

    edit location home

--- toolmanagementsystem/toolsmanagementsystem/src/Position/Manager/Pages/ManageProjects/Projects/Location/LocationHome.jsx
+++ toolmanagementsystem/toolsmanagementsystem/src/Position/Manager/Pages/ManageProjects/Projects/Location/LocationHome.jsx
@@ -1,58 +1,64 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import {Link} from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom';
 import Sidebar from '../../../../../../Components/ManagerSidebar.jsx';
 
 export default function LocationHome() {
 
-    const [location,setlocation]=useState([])
-    useEffect (()=>{
-      loadLocations();
-    },[])
-    
-    const loadLocations=async()=>{
-      const result=await axios.get("http://localhost:8080/locations")
-      setlocation(result.data)
+  const [locations, setLocations] = useState([]);
+  
+  useEffect (()=>{
+    loadLocations();
+  },[]);
+  const { locationId } = useParams();  
+
+  const loadLocations = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/locations");
+      setLocations(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
     }
+  };
 
   return (
     <div className="container-fluid">
-      <div className='py-4'>
-      <div className="col-lg-3">
-          <Sidebar/>
+      <div className="row">
+        <div className="col-lg-3">
+          <Sidebar />
         </div>
         <div className="col-lg-9">
-          <div className="py-4">
-              <table className="table border shadow" style={{ maxHeight: '80vh', overflowY: 'auto', maxWidth: '1000px' }} >
-                 <thead>
-                     <tr>
-                          <th scope="col">#</th>
-                          <th scope="col">location Name</th>  
-                          <th scope="col">location Id</th> 
-                          <Link className='btn btn-outline-danger mx-2'to="/AddLocation">Cancel</Link>
-                      </tr>
-                  </thead>
-                  <tbody>
-                        {
-                          location.map((location,index)=>(
-                          <tr>
-                          <th scope="row"key={index}>{index+1}</th>
-                          <td>{location.locationName}</td>
-                          <td>{location.locationId} </td>
+          <br />
+          <h2>Location Details</h2>
+          <div className="py-4" style={{ maxHeight: '80vh', overflowY: 'auto', maxWidth: '1100px' }}>
+            <table className="table border shadow">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Location Name</th>
+                  <th scope="col">Location Id</th>
+                  <th scope="col">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {locations.map((location, index) => (
+                  <tr key={index}>
+                    <th scope="row">{index + 1}</th>
+                    <td>{location.locationName}</td>
+                    <td>{location.locationId}</td>
+                    <td>
+                      <Link className='btn btn-outline-primary mx-2' to={`/UpdateLocation/${location.locationId}`}>Edit</Link>
+                      <button className='btn btn-danger mx-2'>Delete</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <Link className='btn btn-outline-danger mx-2' to="/AddLocation">Back</Link>
 
-                          <td>
-                              <button className='btn btn-outline-primary mx-2'>Edit</button>
-                              <button className='btn btn-danger mx-2'>Delete</button>
-                          </td>
-                          </tr>
-                          ))
-                        } 
-                  </tbody>
-               </table>
-           </div>
+        </div>
       </div>
-      </div>
-      </div>
-    
-  )
+    </div>
+  );
 }
