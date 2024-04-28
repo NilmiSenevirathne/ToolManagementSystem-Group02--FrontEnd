b4632cd273583edfe6753652b317c8783860daa4
NilmiSenevirathne <126900940+NilmiSenevirathne@users.noreply.github.com>
2024-04-28 19:11:27 +0530

    modified toolbox.jsx

--- toolmanagementsystem/toolsmanagementsystem/src/Position/StockSupervisor/ToolBox/CreateToolBox/CreateToolbox.jsx
+++ toolmanagementsystem/toolsmanagementsystem/src/Position/StockSupervisor/ToolBox/CreateToolBox/CreateToolbox.jsx
@@ -1,21 +1,59 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import StockSidebar from '../../../../Components/Sidebar/StockSidebar.jsx';
 import './toolbox.css';
 
 export default function AddToolbox() {
   let navigate = useNavigate();
+  const [locations , setLocations] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [projects, setProjects] = useState ([]);
+
+  useEffect(() =>{
+     fetchLocations();
+     fetchUsers();
+     fetchProjects();
+  },[]);
+
+  //function to the fetch projects
+  const fetchProjects = async () =>{
+    try{
+        const response  = await axios.get("http://localhost:8080/Projects");
+        setProjects(response.data);
+    }catch(error){
+      console.error("Error fetching projects: ", error);
+    }
+  };
+
+  //function to the fetch locations
+  const fetchLocations = async () =>{
+      try{
+        const response  = await axios.get("http://localhost:8080/locations");
+        setLocations(response.data);
+      } catch(error) {
+        console.error("Error fetching locations: ", error);
+      }
+  };
+
+// Function to fetch users
+  const fetchUsers = async () => { 
+        try {
+      const response = await axios.get("http://localhost:8080/authentication/getUsertoolbox");
+      setUsers(response.data);
+    } catch (error) {
+      console.error("Error fetching users: ", error);
+    }
+  };
 
   const [toolbox, setToolbox] = useState({
-    toolboxId:"",
-    projectId:"",
-    projectName:"",
-    sitesupervisorId:"",
-    sitesupervisorName:"",
-    Location:"",
-    Tool:"",
+    toolboxId: "",
+    projectId: "",
+    sitesupervisorId: "",
+    locationId: "", 
+    Tool: "",
   });
+  
 
   const { toolboxId, projectId, projectName, sitesupervisorId, sitesupervisorName, Location, Tool } = toolbox;
 
@@ -61,73 +99,65 @@ export default function AddToolbox() {
 
           <div className="mb-3">
             <label htmlFor="projectId" className="form-label">
-              Project ID
+              Project
             </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Enter project id"
-              name="projectId"
-              value={projectId}
-              onChange={(e) => onInputChange(e)}
-            />
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="projectName" className="form-label">
-              Project Name
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Enter project name"
-              name="projectName"
-              value={projectName}
-              onChange={(e) => onInputChange(e)}
-            />
+             <select
+                 className="form-control"
+                 name="projectId"
+                 value={toolbox.projectId}
+                 onChange={(e) => onInputChange(e)}
+            >
+                <option value="">Select project</option>
+                    {projects.map((project) => (
+                   <option key={project.projectId} value={project.projectId}>
+                      {project.projectName}
+                  </option>
+               ))}
+             </select>
           </div>
 
           <div className="mb-3">
             <label htmlFor="sitesupervisorId" className="form-label">
-              Site Supervisor ID
+              Site Supervisor
             </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Enter site supervisor id"
-              name="sitesupervisorId"
-              value={sitesupervisorId}
-              onChange={(e) => onInputChange(e)}
-            />
+            <select
+                 className="form-control"
+                 name="sitesupervisorId"
+                 value={toolbox.sitesupervisorId}
+                 onChange={(e) => onInputChange(e)}
+            >
+                <option value="">Select Name</option>
+                    {users.map((user) => (
+                   <option key={user.userid} value={user.userid}>
+                      {user.firstname} {user.lastname}
+                  </option>
+               ))}
+            </select>
           </div>
 
-          <div className="mb-3">
-            <label htmlFor="sitesupervisorName" className="form-label">
-              Site Supervisor Name
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Enter site supervisor name"
-              name="sitesupervisorName"
-              value={sitesupervisorName}
-              onChange={(e) => onInputChange(e)}
-            />
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="Location" className="form-label">
+  
+        <div>
+            <label htmlFor="location" className="form-label">
               Location
             </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Enter location"
-              name="Location"
-              value={Location}
-              onChange={(e) => onInputChange(e)}
-            />
-          </div>
+          <select
+               className="form-control"
+               name="locationId"
+               value={toolbox.locationId} 
+               onChange={(e) => onInputChange(e)}
+          >
+               <option value="">Select Location</option>
+              {locations.map((loc) => (
+              <option key={loc.locationId} value={loc.locationId}>
+              {loc.locationName}
+             </option>
+               ))}
+         </select>
+         </div>
+         
+         <div><Link to ='/tool'><button className='selecttool'>SelectTools</button></Link></div>
+         
+         
 
           <button type="submit" className="submit">
             Submit
