36d57e21134482fbbbd20f1813e415b87cfcac61
anusari <116504727+anusari@users.noreply.github.com>
2024-04-30 15:01:23 +0530

    Update AddProjects.jsx

--- toolmanagementsystem/toolsmanagementsystem/src/Position/Manager/Pages/ManageProjects/Projects/AddProjects.jsx
+++ toolmanagementsystem/toolsmanagementsystem/src/Position/Manager/Pages/ManageProjects/Projects/AddProjects.jsx
@@ -57,7 +57,7 @@ export default function AddProjects() {
   return (
     <div className='container-fluid'>
       <div className='row'> 
-        <div className="col-lg-4">
+        <div className="col-lg-3">
           <Sidebar/>
         </div>
         <div className="col-lg-6">
