0a860625cf5b93b26f739ca4d23e78fa7ee8f371
anusari <116504727+anusari@users.noreply.github.com>
2024-05-19 23:25:00 +0530

    Update ViewInventory.jsx

--- toolmanagementsystem/toolsmanagementsystem/src/Position/Manager/Pages/ViewInventory.jsx
+++ toolmanagementsystem/toolsmanagementsystem/src/Position/Manager/Pages/ViewInventory.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
 const CreateToolbox = () =>{
+     const fName="manji";
+     const LName ="anusari";
 
+     const getFullName =(f,n) =>{
+          return `${f},${n}`;
+     }
+
+     const arr =["apple","orange","mango"];
     return(
       
          <div>
+          <p>fullname :{getFullName("manuji" ,"jhon")}</p>
               <h1> Create Toolbox </h1>
+          <p>{fName} like to eat {arr[0
+               
+          ]}</p>
          </div>
      
     );
