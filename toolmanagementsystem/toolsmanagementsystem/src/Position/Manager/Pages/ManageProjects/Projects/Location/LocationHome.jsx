58fc1aa9195ebfc5e4ecf23b91045099d6a5b616
anusari <116504727+anusari@users.noreply.github.com>
2024-07-09 16:59:55 +0530

    Update LocationHome.jsx

--- toolmanagementsystem/toolsmanagementsystem/src/Position/Manager/Pages/ManageProjects/Projects/Location/LocationHome.jsx
+++ toolmanagementsystem/toolsmanagementsystem/src/Position/Manager/Pages/ManageProjects/Projects/Location/LocationHome.jsx
@@ -3,18 +3,17 @@ import axios from 'axios';
 import { Link, useParams } from 'react-router-dom';
 import ManagerSidebar from '../../../../../../Components/ManagerSidebar.jsx';
 import './LocationHome.css';
-import {Grid , Table, TableHead, TableRow, TableCell, TableBody, Container, Typography, Box} from '@mui/material';
+import { Grid, Container, Box, Typography, Button } from '@mui/material';
 import ManagerNavbar from '../../../../../../Components/Navbar/ManagerNavbar.jsx';
 
 export default function LocationHome() {
-
   const [locations, setLocations] = useState([]);
-  
-  useEffect (()=>{
+
+  useEffect(() => {
     loadLocations();
-  },[]);
-  
-  const { locationId } = useParams();  
+  }, []);
+
+  const { locationId } = useParams();
 
   const loadLocations = async () => {
     try {
@@ -29,56 +28,47 @@ export default function LocationHome() {
     const confirmDelete = window.confirm("Are you sure you want to delete this location?");
     if (confirmDelete) {
       await axios.delete(`http://localhost:8080/location/${locationId}`);
-      loadLocations();
+      loadLocations(); // Refresh the locations list after deletion
     }
   };
 
-
   return (
-    
     <Grid container>
-    <Grid item >
-        <ManagerSidebar/>
-    </Grid>
+      <Grid item>
+        <ManagerSidebar />
+      </Grid>
 
-    <Grid item xs>
-        <ManagerNavbar/>
-
+      <Grid item xs>
+        <ManagerNavbar />
         <Container maxWidth="lg">
-      <Box mt={4}>
-        
-        
-        {/* Render error message if error state is set */}
-        {/* {error && <Typography color="error" align="center">{error}</Typography>} */}
-        <Link className="btn" style={{ backgroundColor: 'navy', color: 'white' }} to="/AddLocation">Add Locations</Link>
-
-        <table className="table border shadow">
-        <thead style={{ top: 0, zIndex: 1, background: '#fff' }}>
+          <Box mt={4}>
+            <Link className="btn" style={{ backgroundColor: 'navy', color: 'white' }} to="/AddLocation">Add Locations</Link>
+            <table className="table border shadow">
+              <thead style={{ top: 0, zIndex: 1, background: '#fff' }}>
                 <tr>
                   <th scope="col">#</th>
-                  <th scope="col">Location Name</th>
                   <th scope="col">Location Id</th>
+                  <th scope="col">Location Name</th> 
                   <th scope="col">Actions</th>
                 </tr>
               </thead>
               <tbody>
                 {locations.map((location, index) => (
                   <tr key={index}>
-                    <th scope="row">{index + 1}</th>
-                    <td>{location.locationName}</td>
+                    <th scope="row">{index + 1}</th> 
                     <td>{location.locationId}</td>
+                    <td>{location.locationName}</td>
                     <td>
                       <Link className='btn btn-outline-primary mx-2' to={`/UpdateLocation/${location.locationId}`}>Edit</Link>
-                      <button className='btn btn-danger mx-2' onClick={()=>deleteLocations(location.locationId)}>Delete</button>
+                      <button className='btn btn-danger mx-2' onClick={() => deleteLocations(location.locationId)}>Delete</button>
                     </td>
                   </tr>
                 ))}
               </tbody>
             </table>
-      </Box>
-    </Container>
+          </Box>
+        </Container>
+      </Grid>
     </Grid>
-</Grid>
-   
   );
 }
